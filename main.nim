060787261f1c3acdfd88a7acf4339a66f63a415a
alfiankan <alfiankan19@gmail.com>
2022-11-19 16:41:47 +0700

    feat(body): parse request body

--- main.nim
+++ main.nim
@@ -1,26 +1,39 @@
-import std/[asyncnet, asyncdispatch, strutils]
+import std/[asyncnet, asyncdispatch, strutils, tables]
 
-type HttpHeader = ref object of RootObj
-    name, value: string
-
-type HttpRequest = ref object of RootObj
+type HttpRequest* = ref object of RootObj
     httpMethod, httpUri : string
-    headers: seq[HttpHeader]
+    headers: Table[string, string]
+    body: string
 
-method setHttpMethod(r: HttpRequest, incomingMethod: string) {.base.} =
+proc newRequest(): HttpRequest =
+    var request = HttpRequest()
+    request.headers = initTable[string, string]()
+    return request
+
+method setHttpMethod*(r: HttpRequest, incomingMethod: string) {.base.} =
     r.httpMethod = incomingMethod
 
-method setHttpUri(r: HttpRequest, incomingHttpUri: string) {.base.} =
+method setHttpUri*(r: HttpRequest, incomingHttpUri: string) {.base.} =
     r.httpUri = incomingHttpUri
 
-method addHeader(r: HttpRequest, name: string, value: string) {.base.} =
-   let header: HttpHeader = HttpHeader()
-   header.name = name
-   header.value = value
-   r.headers.add(header)
+method addHeader*(r: HttpRequest, name: string, value: string) {.base.} =
+    r.headers[name] = ""
+    r.headers[name].add(value)
+
+method getBodyContentLength*(r: HttpRequest): (bool, string) =
+    # find content length
+    let exist = r.headers.hasKey("Content-Length")
+    if exist:
+        return (true, r.headers["Content-Length"])
+    else:
+        return (false, "")
+
+method setBodyContent*(r: HttpRequest, body: string) {.base.} =
+    r.body =  body
+    
 
 proc processClientRequest(client: AsyncSocket) {.async.} =
-    var req = HttpRequest()
+    var req = newRequest()
     
     # get top head
     var lineBuffer: string
@@ -38,8 +51,14 @@ proc processClientRequest(client: AsyncSocket) {.async.} =
         #echo splitedHeader
         req.addHeader(splitedHeader[0], splitedHeader[1])
 
+    # read body
+    let (bodyExist, bodyLength) = req.getBodyContentLength()
+    if bodyExist:
+        lineBuffer = await client.recv(parseInt(bodyLength))
+        req.setBodyContent(lineBuffer)
 
-    echo "Request coming with ", req.httpMethod, "method", req.headers[0].name
+    
+    echo "Request coming with ", req.httpMethod, "method and body ", req.body
 
     await client.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<p>hello anemian</p>\r\n")
     client.close()
