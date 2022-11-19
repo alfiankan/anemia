54f7fb29ce974754b1f319c5a8df5ed8343c3b27
alfiankan <alfiankan19@gmail.com>
2022-11-19 16:07:37 +0700

    feat(header): populating headers

--- main.nim
+++ main.nim
@@ -1,7 +1,11 @@
 import std/[asyncnet, asyncdispatch, strutils]
 
+type HttpHeader = ref object of RootObj
+    name, value: string
+
 type HttpRequest = ref object of RootObj
     httpMethod, httpUri : string
+    headers: seq[HttpHeader]
 
 method setHttpMethod(r: HttpRequest, incomingMethod: string) {.base.} =
     r.httpMethod = incomingMethod
@@ -9,16 +13,33 @@ method setHttpMethod(r: HttpRequest, incomingMethod: string) {.base.} =
 method setHttpUri(r: HttpRequest, incomingHttpUri: string) {.base.} =
     r.httpUri = incomingHttpUri
 
+method addHeader(r: HttpRequest, name: string, value: string) {.base.} =
+   let header: HttpHeader = HttpHeader()
+   header.name = name
+   header.value = value
+   r.headers.add(header)
+
 proc processClientRequest(client: AsyncSocket) {.async.} =
     var req = HttpRequest()
     
     # get top head
-    var headLine = await client.recvLine()
-    let headArrSplited = headLine.split(" ")
+    var lineBuffer: string
+    lineBuffer = await client.recvLine()
+    let headArrSplited = lineBuffer.split(" ")
     req.setHttpMethod(headArrSplited[0])
     req.setHttpUri(headArrSplited[1])
 
-    echo "Request coming with ", req.httpMethod, "method"
+    # populating headers
+    while true:
+        lineBuffer = await client.recvLine()
+        if lineBuffer == "\r\n":
+            break
+        let splitedHeader = lineBuffer.split(": ")
+        #echo splitedHeader
+        req.addHeader(splitedHeader[0], splitedHeader[1])
+
+
+    echo "Request coming with ", req.httpMethod, "method", req.headers[0].name
 
     await client.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<p>hello anemian</p>\r\n")
     client.close()
